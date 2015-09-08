583664e82c0517a983eaa93509f8f585d1176027
nwshane <django09@gmail.com>
2015-09-08 11:16:12 +0400

    Created PlaceGeAd class and refactored accordingly.

--- lib/place_ge_scraper.rb
+++ lib/place_ge_scraper.rb
@@ -1,10 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
 
-module PlaceGeScraper
-  def self.ad_price
-    page = Nokogiri::HTML(open("http://place.ge/en/ads/view/459002"))
-    price_content = page.css('.top-ad .price')[0].content
+class PlaceGeAd
+  def initialize(uri)
+    @uri = uri
+    @page = Nokogiri::HTML(open(@uri))
+    @price = scrape_price
+    @area = scrape_area
+  end
+
+  def to_s
+    "Uri: #{@uri}\nPrice: #{@price}\nArea: #{@area}"
+  end
+
+  def scrape_price
+    price_content = @page.css('.top-ad .price')[0].content
     price = price_content[/\$.*\//]
               .gsub("\t", '')
               .gsub('/', '')
@@ -13,9 +23,8 @@ module PlaceGeScraper
               .to_i
   end
 
-  def self.area_square_meters
-    page = Nokogiri::HTML(open("http://place.ge/en/ads/view/459002"))
-    area_whole_text = page.xpath("//*[contains(concat(' ', @class, ' '), ' detailBox2 ' )][contains(., 'Space')]//*[contains(concat(' ', @class, ' '), ' detailRight ')]/text()").text
+  def scrape_area
+    area_whole_text = @page.xpath("//*[contains(concat(' ', @class, ' '), ' detailBox2 ' )][contains(., 'Space')]//*[contains(concat(' ', @class, ' '), ' detailRight ')]/text()").text
     area = area_whole_text.gsub!(/[^0-9]/, '').to_i
   end
 end
--- lib/tasks/place_ge_scraper.rake
+++ lib/tasks/place_ge_scraper.rake
@@ -1,12 +1,10 @@
 namespace :place_ge_scraper do
   desc 'Get price'
-  task get_price: :environment do
+  task :scrape_ad, [:uri] => :environment do |_t, args|
+    uri = args[:uri]
     require 'place_ge_scraper'
-    puts "Ad price: #{PlaceGeScraper.ad_price}"
-  end
 
-  task get_area: :environment do
-    require 'place_ge_scraper'
-    puts "Area square meters: #{PlaceGeScraper.area_square_meters}"    
+    ad = PlaceGeAd.new(uri)
+    puts ad.to_s
   end
 end

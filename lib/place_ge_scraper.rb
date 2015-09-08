c05e2bb01c800846991f9858c6dabcba69993e4b
nwshane <django09@gmail.com>
2015-09-08 10:53:06 +0400

    Added area square meters scraper function.

--- lib/place_ge_scraper.rb
+++ lib/place_ge_scraper.rb
@@ -12,4 +12,10 @@ module PlaceGeScraper
               .gsub('$', '')
               .to_i
   end
+
+  def self.area_square_meters
+    page = Nokogiri::HTML(open("http://place.ge/en/ads/view/459002"))
+    area_whole_text = page.xpath("//*[contains(concat(' ', @class, ' '), ' detailBox2 ' )][contains(., 'Space')]//*[contains(concat(' ', @class, ' '), ' detailRight ')]/text()").text
+    area = area_whole_text.gsub!(/[^0-9]/, '').to_i
+  end
 end
--- lib/tasks/place_ge_scraper.rake
+++ lib/tasks/place_ge_scraper.rake
@@ -4,4 +4,9 @@ namespace :place_ge_scraper do
     require 'place_ge_scraper'
     puts "Ad price: #{PlaceGeScraper.ad_price}"
   end
+
+  task get_area: :environment do
+    require 'place_ge_scraper'
+    puts "Area square meters: #{PlaceGeScraper.area_square_meters}"    
+  end
 end

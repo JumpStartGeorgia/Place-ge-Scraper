e0bb674419eddbba67c7bcb74d73113ad07b5784
nwshane <django09@gmail.com>
2015-09-08 11:41:58 +0400

    Refactored xpath and gsub helper methods.

--- lib/place_ge_scraper.rb
+++ lib/place_ge_scraper.rb
@@ -2,9 +2,29 @@ require 'nokogiri'
 require 'open-uri'
 
 class PlaceGeAd
+  def scrape_price
+    price_content = @page.css('.top-ad .price')[0].content
+    price_content[/\$.*\//]
+      .gsub("\t", '')
+      .gsub('/', '')
+      .gsub(',', '')
+      .gsub('$', '')
+      .to_i
+  end
+
+  def scrape_area
+    area_whole_text = detail_value(@page, 'Space')
+    area_whole_text.remove_non_numbers.to_i
+  end
+
+  def detail_value(page, detail_label)
+    page.xpath("//*[contains(concat(' ', @class, ' '), ' detailBox2 ' )][contains(., '#{detail_label}')]//*[contains(concat(' ', @class, ' '), ' detailRight ')]/text()").text
+  end
+
   def initialize(uri)
     @uri = uri
     @page = Nokogiri::HTML(open(@uri))
+
     @price = scrape_price
     @area = scrape_area
   end
@@ -12,19 +32,10 @@ class PlaceGeAd
   def to_s
     "\nScraping place.ge! Real Estate Ad Uri: #{@uri}\n------------------------------------------------------\nPrice: #{@price}\nArea: #{@area}"
   end
+end
 
-  def scrape_price
-    price_content = @page.css('.top-ad .price')[0].content
-    price = price_content[/\$.*\//]
-              .gsub("\t", '')
-              .gsub('/', '')
-              .gsub(',', '')
-              .gsub('$', '')
-              .to_i
-  end
-
-  def scrape_area
-    area_whole_text = @page.xpath("//*[contains(concat(' ', @class, ' '), ' detailBox2 ' )][contains(., 'Space')]//*[contains(concat(' ', @class, ' '), ' detailRight ')]/text()").text
-    area = area_whole_text.gsub!(/[^0-9]/, '').to_i
+class String
+  def remove_non_numbers
+    self.gsub!(/[^0-9]/, '')
   end
 end

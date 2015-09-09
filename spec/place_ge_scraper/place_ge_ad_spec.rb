e02414e61985de653c44a6625fe361871918f4e3
nwshane <django09@gmail.com>
2015-09-09 10:40:07 +0400

    Refactored place_ge_scraper spec.

--- spec/place_ge_scraper/place_ge_ad_spec.rb
+++ spec/place_ge_scraper/place_ge_ad_spec.rb
@@ -2,13 +2,13 @@ require 'place_ge_scraper'
 
 describe 'PlaceGeAd' do
   describe 'ID 459002' do
+    let!(:ad) { PlaceGeAd.new('http://place.ge/en/ads/view/459002') }
+
     it 'price is correct' do
-      ad = PlaceGeAd.new('http://place.ge/en/ads/view/459002')
       expect(ad.price).to eq(46000)
     end
 
     it 'size is correct' do
-      ad = PlaceGeAd.new('http://place.ge/en/ads/view/459002')
       expect(ad.size).to eq(56)
     end
   end

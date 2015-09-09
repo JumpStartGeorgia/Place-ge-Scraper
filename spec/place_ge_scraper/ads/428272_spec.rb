011136f67be1b6f5437a204c96a3da30c447bd3d
nwshane <django09@gmail.com>
2015-09-09 12:09:16 +0400

    Added nil values for place ge ad 428272 test.

--- spec/place_ge_scraper/ads/428272_spec.rb
+++ spec/place_ge_scraper/ads/428272_spec.rb
@@ -1,6 +1,13 @@
 test_place_ge_ad(428272, {
   price: 280000,
+  size: nil,
+  size_unit: nil,
   renovation_type: 'Renovated',
   address: 'Bakuriani',
-
+  city: nil,
+  area: nil,
+  district: nil,
+  street: nil,
+  building_number: nil,
+  apartment_number: nil
 })

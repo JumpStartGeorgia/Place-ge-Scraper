ce36853b5524afb9d395bf64a8583893387a6a2d
nwshane <django09@gmail.com>
2015-09-09 11:34:51 +0400

    Refactored place ge scraper specs for easy testing of ads.

--- spec/place_ge_scraper/place_ge_ad_spec.rb
+++ spec/place_ge_scraper/place_ge_ad_spec.rb
@@ -1,53 +1,69 @@
 require 'place_ge_scraper'
 
-describe 'PlaceGeAd' do
-  describe 'ID 459002' do
-    before :context do
-      @ad = PlaceGeAd.new('http://place.ge/en/ads/view/459002')
-    end
+def test_place_ge_ad(id, expected)
+  describe 'PlaceGeAd' do
+    describe "ID #{id}" do
+      before :context do
+        @ad = PlaceGeAd.new("http://place.ge/en/ads/view/#{id}")
+      end
 
-    it 'price is correct' do
-      expect(@ad.price).to eq(46000)
-    end
+      it 'price is correct' do
+        expect(@ad.price).to eq(expected[:price])
+      end
 
-    it 'size is correct' do
-      expect(@ad.size).to eq(56)
-    end
+      it 'size is correct' do
+        expect(@ad.size).to eq(expected[:size])
+      end
 
-    it 'size_unit is correct' do
-      expect(@ad.size_unit).to eq('sq. m.')
-    end
+      it 'size_unit is correct' do
+        expect(@ad.size_unit).to eq(expected[:size_unit])
+      end
 
-    it 'renovation_type is correct' do
-      expect(@ad.renovation_type).to eq('Old renovated')
-    end
+      it 'renovation_type is correct' do
+        expect(@ad.renovation_type).to eq(expected[:renovation_type])
+      end
 
-    it 'address is correct' do
-      expect(@ad.address).to eq('Tbilisi, Vake-Saburtalo, Saburtalo, Kavtaradze st.')
-    end
+      it 'address is correct' do
+        expect(@ad.address).to eq(expected[:address])
+      end
 
-    it 'city is correct' do
-      expect(@ad.city).to eq('Tbilisi')
-    end
+      it 'city is correct' do
+        expect(@ad.city).to eq(expected[:city])
+      end
 
-    it 'area is correct' do
-      expect(@ad.area).to eq('Vake-Saburtalo')
-    end
+      it 'area is correct' do
+        expect(@ad.area).to eq(expected[:area])
+      end
 
-    it 'district is correct' do
-      expect(@ad.district).to eq('Saburtalo')
-    end
+      it 'district is correct' do
+        expect(@ad.district).to eq(expected[:district])
+      end
 
-    it 'street is correct' do
-      expect(@ad.street).to eq('Kavtaradze st.')
-    end
+      it 'street is correct' do
+        expect(@ad.street).to eq(expected[:street])
+      end
 
-    it 'building_number is correct' do
-      expect(@ad.building_number).to eq('13')
-    end
+      it 'building_number is correct' do
+        expect(@ad.building_number).to eq(expected[:building_number])
+      end
 
-    it 'apartment_number is correct' do
-      expect(@ad.apartment_number).to eq('6')
+      it 'apartment_number is correct' do
+        expect(@ad.apartment_number).to eq(expected[:apartment_number])
+      end
     end
   end
 end
+
+test_place_ge_ad(459002, {
+  price: 46000,
+  size: 56,
+  size_unit: 'sq. m.',
+  renovation_type: 'Old renovated',
+  address: 'Tbilisi, Vake-Saburtalo, Saburtalo, Kavtaradze st.',
+  city: 'Tbilisi',
+  area: 'Vake-Saburtalo',
+  district: 'Saburtalo',
+  street: 'Kavtaradze st.',
+  building_number: '13',
+  apartment_number: '6'
+})

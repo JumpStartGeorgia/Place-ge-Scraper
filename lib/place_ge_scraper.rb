6f2138f38ad44c34329d5f96b67b7f087911e985
nwshane <django09@gmail.com>
2015-09-10 12:25:09 +0400

    Added getters to place ge scraper.

--- lib/place_ge_scraper.rb
+++ lib/place_ge_scraper.rb
@@ -51,9 +51,9 @@ class PlaceGeAd
     @page.detail_value('Appartment No.')
   end
 
-  def initialize(uri)
-    @uri = uri
-    @page = Nokogiri::HTML(open(@uri))
+  def initialize(link)
+    @link = link
+    @page = Nokogiri::HTML(open(@link))
 
     @price = scrape_price
     @size = scrape_size
@@ -73,40 +73,116 @@ class PlaceGeAd
     "\nScraping place.ge! Real Estate Ad Uri: #{@uri}\n------------------------------------------------------\nPrice: #{@price}\nSize: #{@size}\nSize unit: #{@size_unit}\nRenovation: #{@renovation_type}\n\nAddress: #{@address}\nCity: #{@city}\nArea: #{@area}\nDistrict: #{@district}\nStreet: #{@street}\nBuilding number: #{@building_number}\nApartment number: #{@apartment_number}"
   end
 
-  def price
-    @price
+  def place_ge_id
+    @place_ge_id
   end
 
-  def size
-    @size
+  def link
+    @link
   end
 
-  def size_unit
-    @size_unit
+  def publication_date
+    @publication_date
   end
 
-  def renovation_type
-    @renovation_type
+  def deal_type
+    @deal_type
   end
 
-  def address
-    @address
+  def property_type
+    @property_type
+  end
+
+  def city_id
+    @city_id
   end
 
   def city
     @city
   end
 
-  def area
-    @area
+  def region_id
+    @region_id
+  end
+
+  def region
+    @region
+  end
+
+  def district_id
+    @district_id
   end
 
   def district
     @district
   end
 
+  def street_id
+    @street_id
+  end
+
   def street
     @street
+  end
+
+  def price
+    @price
+  end
+
+  def price_per_area_unit
+    @price_per_area_unit
+  end
+
+  def price_currency
+    @price_currency
+  end
+
+  def area
+    @area
+  end
+
+  def area_unit
+    @area_unit
+  end
+
+  def land_area
+    @land_area
+  end
+
+  def land_area_unit
+    @land_area_unit
+  end
+
+  def distance_from_tbilisi
+    @distance_from_tbilisi
+  end
+
+  def distance_from_main_road
+    @distance_from_main_road
+  end
+
+  def condition
+    @condition
+  end
+
+  def project
+    @project
+  end
+
+  def status
+    @status
+  end
+
+  def array
+    @array
+  end
+
+  def quarter
+    @quarter
+  end
+
+  def neighborhood
+    @neighborhood
   end
 
   def building_number
@@ -115,6 +191,158 @@ class PlaceGeAd
 
   def apartment_number
     @apartment_number
+  end
+
+  def address
+    @address
+  end
+
+  def floor_number
+    @floor_number
+  end
+
+  def total_floor_count
+    @total_floor_count
+  end
+
+  def room_count
+    @room_count
+  end
+
+  def bathroom_count
+    @bathroom_count
+  end
+
+  def bedroom_count
+    @bedroom_count
+  end
+
+  def balcony_count
+    @balcony_count
+  end
+
+  def is_bank_real_estate
+    @is_bank_real_estate
+  end
+
+  def has_garage_or_parking
+    @has_garage_or_parking
+  end
+
+  def has_lift
+    @has_lift
+  end
+
+  def has_furniture
+    @has_furniture
+  end
+
+  def has_fireplace
+    @has_fireplace
+  end
+
+  def has_storage_area
+    @has_storage_area
+  end
+
+  def has_wardrobe
+    @has_wardrobe
+  end
+
+  def has_air_conditioner
+    @has_air_conditioner
+  end
+
+  def has_heating
+    @has_heating
+  end
+
+  def has_loggia
+    @has_loggia
+  end
+
+  def has_technology
+    @has_technology
+  end
+
+  def has_hot_water
+    @has_hot_water
+  end
+
+  def has_tv
+    @has_tv
+  end
+
+  def has_phone
+    @has_phone
+  end
+
+  def has_internet
+    @has_internet
+  end
+
+  def has_alarm
+    @has_alarm
+  end
+
+  def has_doorphone
+    @has_doorphone
+  end
+
+  def has_security
+    @has_security
+  end
+
+  def has_conference_hall
+    @has_conference_hall
+  end
+
+  def has_showcase
+    @has_showcase
+  end
+
+  def has_veranda
+    @has_veranda
+  end
+
+  def is_mansard
+    @is_mansard
+  end
+
+  def has_electricity
+    @has_electricity
+  end
+
+  def has_gas
+    @has_gas
+  end
+
+  def has_water
+    @has_water
+  end
+
+  def has_sewage
+    @has_sewage
+  end
+
+  def has_inventory
+    @has_inventory
+  end
+
+  def has_network
+    @has_network
+  end
+
+  def has_generator
+    @has_generator
+  end
+
+  def additional_information
+    @additional_information
+  end
+
+  def telephone_number
+    @telephone_number
   end
 end
 

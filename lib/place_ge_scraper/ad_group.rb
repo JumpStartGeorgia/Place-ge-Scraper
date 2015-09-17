6ac410c52d4a4beef12a9fc1491a6ca588c3fd95
nwshane <django09@gmail.com>
2015-09-17 12:24:20 +0400

    Added fields is_vip, is_paid, and is_simple to PlaceGeAdBox class.

--- lib/place_ge_scraper/ad_group.rb
+++ lib/place_ge_scraper/ad_group.rb
@@ -7,16 +7,17 @@ require_relative 'helper'
 # Group of place.ge real estate ads
 class PlaceGeAdGroup
   def initialize
-    @date = Date.today - 1
+    @start_date = Date.today - 7
+    @end_date = Date.today
 
-    @ad_ids = get_ad_ids
+    @ad_ids = scrape_ad_ids
   end
 
   def to_s
     puts @ad_ids
   end
 
-  def get_ad_ids
+  def scrape_ad_ids
     link = "http://place.ge/ge/ads/page:1?object_type=all&currency_id=2&mode=list&order_by=date&limit=30"
     page = Nokogiri.HTML(open(link))
 
@@ -25,6 +26,9 @@ class PlaceGeAdGroup
 
     ad_boxes.each do |ad_box_html|
       ad_box = PlaceGeAdBox.new(ad_box_html)
+      puts 'VIP' if ad_box.is_vip?
+      puts 'PAID' if ad_box.is_paid?
+      puts 'SIMPLE' if ad_box.is_simple?
       process_ad_box(ad_box, ad_ids)
     end
 
@@ -32,11 +36,10 @@ class PlaceGeAdGroup
   end
 
   def process_ad_box(ad_box, ad_ids)
-    if ad_box.pub_date == @date
-      unless ad_ids.include? ad_box.id
-        ad_ids.push(ad_box.id)
-      end
-    end
+    return unless ad_box.pub_date == @date
+    return if ad_ids.include? ad_box.id
+
+    ad_ids.push(ad_box.id)
   end
 end
 
@@ -54,6 +57,18 @@ class PlaceGeAdBox
     Date.strptime(pub_date_string, '%d.%m.%Y')
   end
 
+  def scrape_is_vip
+    @html.attributes['class'].value.include? 'vip-ad'
+  end
+
+  def scrape_is_paid
+    @html.attributes['class'].value.include? 'paid-ad'
+  end
+
+  def scrape_is_simple
+    @html.attributes['class'].value.include? 'simple-ad'
+  end
+
   def id
     @id = scrape_id if @id.nil?
     @id
@@ -62,5 +77,20 @@ class PlaceGeAdBox
   def pub_date
     @pub_date = scrape_pub_date if @pub_date.nil?
     @pub_date
+  end
+
+  def is_vip?
+    @is_vip = scrape_is_vip if @is_vip.nil?
+    @is_vip
+  end
+
+  def is_paid?
+    @is_paid = scrape_is_paid if @is_paid.nil?
+    @is_paid
+  end
+
+  def is_simple?
+    @is_simple = scrape_is_simple if @is_simple.nil?
+    @is_simple
   end
 end

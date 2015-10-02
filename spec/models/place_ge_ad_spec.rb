a9684c5b3350988472ddc678b22c2d2ef30e603e
nwshane <django09@gmail.com>
2015-10-02 13:37:01 +0400

    Added a PlaceGeAd spec to check that an ad entry is not created if an identical one is in the database.

--- spec/models/place_ge_ad_spec.rb
+++ spec/models/place_ge_ad_spec.rb
@@ -1,5 +1,9 @@
 describe 'PlaceGeAd' do
-  let(:new_place_ge_ad) do
+  let :new_place_ge_ad do
+    PlaceGeAdFactory.build
+  end
+
+  let :other_place_ge_ad do
     PlaceGeAdFactory.build
   end
 
@@ -8,6 +12,18 @@ describe 'PlaceGeAd' do
       expect do
         new_place_ge_ad.save
       end.to change(AdEntry, :count).by(1)
+    end
+
+    context 'when identical place_ge_ad has already been saved' do
+      before :example do
+        other_place_ge_ad.save
+      end
+
+      it 'does not create ad_entry' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(AdEntry, :count).by(0)
+      end
     end
   end
 end

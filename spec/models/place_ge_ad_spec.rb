78ab3ecfb2df1f5cb6aebfb788f1bf6faef88a11
nwshane <django09@gmail.com>
2015-10-05 11:53:04 +0400

    Added more specs to ensure that ad save function works.

--- spec/models/place_ge_ad_spec.rb
+++ spec/models/place_ge_ad_spec.rb
@@ -8,16 +8,61 @@ describe 'PlaceGeAd' do
   end
 
   describe 'save' do
-    it 'creates ad_entry' do
-      expect do
-        new_place_ge_ad.save
-      end.to change(AdEntry, :count).by(1)
-    end
-
     it 'creates ad entry with correct time of scrape' do
       new_saved_entry = new_place_ge_ad.save
       new_saved_entry.reload
       expect(new_saved_entry.time_of_scrape).to eq(new_place_ge_ad.time_of_scrape)
+    end
+
+    context 'when no other ads are saved in database' do
+      it 'creates ad_entry' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(AdEntry, :count).by(1)
+      end
+
+      it 'creates new ad' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(Ad, :count).by(1)
+      end
+    end
+
+    context 'when same place_ge_ad but with different place_ge_id is saved' do
+      let! :saved_entry_different_id do
+        other_place_ge_ad.place_ge_id
+      end
+
+      it 'creates new ad entry' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(AdEntry, :count).by(1)
+      end
+
+      it 'creates new ad' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(Ad, :count).by(1)
+      end
+    end
+
+    context 'when almost identical place_ge_ad but with old publication date is saved' do
+      let! :different_saved_entry do
+        other_place_ge_ad.publication_date = new_place_ge_ad.publication_date + 1
+        other_place_ge_ad.save
+      end
+
+      it 'creates new ad entry' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(AdEntry, :count).by(1)
+      end
+
+      it 'does not create new ad' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(Ad, :count).by(0)
+      end
     end
 
     context 'when identical place_ge_ad has already been saved' do
@@ -29,6 +74,12 @@ describe 'PlaceGeAd' do
         expect do
           new_place_ge_ad.save
         end.to change(AdEntry, :count).by(0)
+      end
+
+      it 'does not create new ad' do
+        expect do
+          new_place_ge_ad.save
+        end.to change(Ad, :count).by(0)
       end
 
       it 'should update time of scrape of identical place_ge_ad' do

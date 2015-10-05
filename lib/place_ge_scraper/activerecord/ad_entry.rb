d78f247e19e52f664c62a71ecfeca18d55a65b1c
nwshane <django09@gmail.com>
2015-10-05 14:05:32 +0400

    Rubocop improvement.

--- lib/place_ge_scraper/activerecord/ad_entry.rb
+++ lib/place_ge_scraper/activerecord/ad_entry.rb
@@ -2,7 +2,6 @@ require_relative '../../../environment'
 
 # An ActiveRecord class for saving data in a place.ge ad entry in a database
 class AdEntry < ActiveRecord::Base
-
   # Gets all attributes except for id and time of scrape, which are unrelated
   # to the actual info on place.ge
   def place_ge_entry_attributes

0e45cc6f9c0c76d780f0b123e908399c9221454a
nwshane <django09@gmail.com>
2015-10-06 13:09:23 +0400

    Added belongs_to :ad relationship to AdEntry activerecord class.

--- lib/place_ge_scraper/activerecord/ad_entry.rb
+++ lib/place_ge_scraper/activerecord/ad_entry.rb
@@ -2,6 +2,8 @@ require_relative '../../../environment'
 
 # An ActiveRecord class for saving data in a place.ge ad entry in a database
 class AdEntry < ActiveRecord::Base
+  belongs_to :ad
+
   # Gets all attributes except for id and time of scrape, which are unrelated
   # to the actual info on place.ge
   def place_ge_entry_attributes
